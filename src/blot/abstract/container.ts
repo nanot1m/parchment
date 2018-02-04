26264e374cf3eca6c3504d8a08c909b328e3f132
Jason Chen <jhchen7@gmail.com>
2018-02-04 01:41:26 -0800

    update dependencies and fix errors

--- src/blot/abstract/container.ts
+++ src/blot/abstract/container.ts
@@ -1,9 +1,9 @@
-import ParentBlot from './parent';
-import BlockBlot from '../block';
-import * as Registry from '../../registry';
+import ParentBlot from "./parent";
+import BlockBlot from "../block";
+import * as Registry from "../../registry";
 
 class ContainerBlot extends ParentBlot {
-  static blotName = 'container';
+  static blotName = "container";
   static scope = Registry.Scope.BLOCK_BLOT;
   static tagName: string;
   // static allowedChildren: Registry.BlotConstructor[] = [BlockBlot, ContainerBlot];
